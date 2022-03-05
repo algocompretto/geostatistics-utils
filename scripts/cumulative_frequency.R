f41129fa5d5ad3253fbfec1b5c198bc2c22829a5
Gustavo Scholze <scholzegustavo@gmail.com>
2022-03-04 22:07:40 -0300

    cumulative frequency

---
+++ scripts/cumulative_frequency.R
@@ -0,0 +1,14 @@
+setwd("..\\data_r\\simetrica225")
+dados <- read.csv("simetrica225.csv",sep=";",header=TRUE)
+variavel = dados$Zgauss
+breaks=c(2.957,5.573,8.189,10.805,13.420,
+16.036,18.652,21.268,23.884,26.500)
+variavel.cut = cut(variavel, breaks, right=FALSE) 
+variavel.freq = table(variavel.cut)
+setwd("..\\data_r\\tempo")
+pdf("Cumulative_frequency.pdf",width=5,height=5)
+cumfreq0 = c(0, cumsum(variavel.freq)) 
+plot(breaks, cumfreq0,main="Zgauss",xlab="Zgauss",
+ylab="Frequência acumulada")
+lines(breaks, cumfreq0)
+dev.off()
